aa6d9885986ba78ab4a230ed23432ec7e3ec416b
Gleb Korolkov <gkorolkov@loyalty.com>
2020-12-26 17:49:12 -0500

    Refactor ControlPanel component.

--- src/components/ControlPanel/ControlPanel.jsx
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
-import './ControlPanel.css'
+import './ControlPanel.scss'
+
+import MultiCheckbox from '../MultiCheckbox'
 
 
 export default class ControlPanel extends React.Component {
@@ -9,21 +11,43 @@ export default class ControlPanel extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-        collapsed: false
+      collapsed: false,
+      schoolTypes: ['Elementary', 'Senior']
     }
     this.toggle = this.toggle.bind(this)
+    this.handleSchoolTypeFilter = this.handleSchoolTypeFilter.bind(this)
   }
 
   toggle() {
     this.setState({collapsed: !this.state.collapsed})
   }
 
+  handleSchoolTypeFilter(fields) {
+    console.log(fields)
+  }
+
   render() {
+    const renderFilters = () => {
+      if (!this.state.collapsed) {
+        return (
+          <div>
+            <h3 className="title is-3">Filters</h3>
+            <h5 className="title is-6 filter-title">School Type</h5>
+            <MultiCheckbox
+              fields={ this.state.schoolTypes.map(schType => ({label: schType, checked: true})) }
+              onChange={ this.handleSchoolTypeFilter }
+            />
+          </div>
+        )
+      }
+    }
     return (
       <div className={ 'ControlPanel' + (this.state.collapsed ? ' collapsed' : '') }>
-        <div className={ 'column has-text-' + (this.state.collapsed ? 'centered' : 'right') }>
+        <div className="column">
+          { renderFilters() }
           <FontAwesomeIcon
-            icon={ this.state.collapsed ? faAngleDoubleRight : faAngleDoubleLeft}
+            className="toggle"
+            icon={this.state.collapsed ? faAngleDoubleRight : faAngleDoubleLeft}
             onClick={this.toggle} />
         </div>
       </div>
