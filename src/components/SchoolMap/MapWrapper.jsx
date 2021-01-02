c92c3e5c7bc284fb27826c36cd9e1683acc322b9
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-02 15:10:19 -0500

    Add display of school catchment area boundaries.

--- src/components/SchoolMap/MapWrapper.jsx
+++ src/components/SchoolMap/MapWrapper.jsx
@@ -7,13 +7,20 @@ export class MapWrapper extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
     return (
       nextProps.children.length !== this.props.children.length ||
-      nextProps.colorFunc !== this.props.colorFunc
+      nextProps.colorFunc !== this.props.colorFunc ||
+      nextProps.schoolBoundaries !== this.props.schoolBoundaries
     )
   }
 
   render() {
+    let allChildren = this.props.children
+    if (this.props.schoolBoundaries)
+      allChildren = [...this.props.children, ...this.props.schoolBoundaries]
+    console.log(this.props.schoolBoundaries)
     return (
-      <Map {...this.props}/>
+      <Map {...this.props}>
+        {allChildren}
+      </Map>
     )
   }
 }
--- src/components/SchoolMap/SchoolMap.jsx
+++ src/components/SchoolMap/SchoolMap.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Marker, GoogleApiWrapper} from 'google-maps-react'
+import {Marker, Polygon, GoogleApiWrapper} from 'google-maps-react'
 import './SchoolMap.css'
 
 import SchoolCard from '../SchoolCard'
 import {MapWrapper} from './MapWrapper'
+
+
+const SCHOOL_AREA_DEFAULT_COLOR = 'green'
 
 
 export class GoogleMapContainer extends React.Component {
@@ -17,6 +20,7 @@ export class GoogleMapContainer extends React.Component {
     this.defaultZoom = 14
     this.handleMarkerClick = this.handleMarkerClick.bind(this)
     this.handleMapClick = this.handleMapClick.bind(this)
+    this.renderBoundaries = this.renderBoundaries.bind(this)
   }
 
   componentDidUpdate(prevProps) {
@@ -43,6 +47,30 @@ export class GoogleMapContainer extends React.Component {
     this.setState({selectedSchool: null})
   }
 
+  renderBoundaries() {
+    const school = this.state.selectedSchool
+    if (!school || !school.boundaries) return ''
+    const prepPaths = 
+      (path) => path.map(
+        segm => segm.map(
+          p => ({lat: p.lat, lng: p.lon})
+        )
+      )
+    const color = SCHOOL_AREA_DEFAULT_COLOR
+    return (
+      [<Polygon
+        key="Catchment area"
+        title={`${school.name} catchment area`}
+        paths={prepPaths(school.boundaries)}
+        strokeColor={color}
+        strokeOpacity={0.8}
+        strokeWeight={2}
+        fillColor={color}
+        fillOpacity={0.2}
+      />]
+    )
+  }
+
   render() {
     return (
       <div className="school-map-container">
@@ -61,7 +89,8 @@ export class GoogleMapContainer extends React.Component {
           streetViewControlOptions={{position: this.props.google.maps.ControlPosition.RIGHT_TOP}}
           zoomControlOptions={{position: this.props.google.maps.ControlPosition.RIGHT_TOP}}
           colorFunc={this.props.colorFunc}
-          onClick={this.handleMapClick}>
+          onClick={this.handleMapClick}
+          schoolBoundaries={this.renderBoundaries()}>
 
           {this.props.schools
             .filter(school => school.coords)
