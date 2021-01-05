6106ba0ca7ef17340c925e1d4a7dfd2b6ba75377
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-05 09:27:32 -0500

    Refactor SchoolCard component as a class to deal with collapsed state persistency issues.

--- src/components/SchoolCard/SchoolCard.jsx
+++ src/components/SchoolCard/SchoolCard.jsx
@@ -8,159 +8,191 @@ import _padStart from 'lodash.padstart'
 import './SchoolCard.scss'
 
 
-const SchoolCard = (props) => {
+const NDASH = '\u2013'
 
-  const [collapsed, setCollapsed] = React.useState(props.collapsed || false)
 
-  React.useEffect(() => {setCollapsed(props.collapsed)}, [props]);
+export default class SchoolCard extends React.Component {
 
-  const toggle = () => {
-    setCollapsed(!collapsed)
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: props.collapsed || false
+    };
+    this.toggle = this.toggle.bind(this);
   }
 
-  const school = props.school
-  if (!school) return ''
-
-  const nDash = '\u2013'
-
-  const quickFacts = (
-    <nav className="level my-2">
-      <div className="level-item has-text-centered">
-        <div>
-          <p className="heading">School Board</p>
-          <p className="title is-7">{school.school_board}</p>
-        </div>
-      </div>
-      <div className="level-item has-text-centered">
-        <div>
-          <p className="heading">Opening Year</p>
-          <p className="title is-7">{school.year || nDash}</p>
-        </div>
-      </div>
-      <div className="level-item has-text-centered">
-        <div>
-          <p className="heading">Fraser Rank</p>
-          <p className="title is-7">
-            {school.fraser ? '#' + Number(school.fraser.rank).toLocaleString() : nDash}
-          </p>
-        </div>
-      </div>
-      <div className="level-item has-text-centered">
-        <div>
-          <p className="heading">Fraser Score</p>
-          <p className="title is-7">{school.fraser ? school.fraser.score.toFixed(1) : nDash}</p>
-        </div>
-      </div>
-    </nav>
-  )
-
-  const fullAddress = school.address ? `${school.address.address}, ${school.address.city}, ${school.address.postal_code}` : nDash
-  const websiteLink = school.website ? (
-    <a href={school.website} target="_blank" rel="noreferrer">{school.website}</a>
-  ) : nDash
-  const extraDetails = (
-    <nav className="level">
-      <div className="level-item is-justify-content-flex-start">
-        <p className="is-size-7"><FontAwesomeIcon icon={faMap} /> {fullAddress}</p>
-      </div>
-      <div className="level-item is-justify-content-flex-start">
-        <p className="is-size-7"><FontAwesomeIcon icon={faGlobe} /> {websiteLink}</p>
-      </div>
-    </nav>
-  )
-
-  const cardContent = collapsed ? '' : (
-    <div className="school-card-content">
-      <hr className="my-2" />
-      {quickFacts}
-      <hr className="my-2" />
-      {extraDetails}
-    </div>
-  )
-
-  const makeGradesLabel = grades => {
-    const formatGrade = (grade) => (isNaN(grade) ? grade : _padStart(grade, 2, '0'))
-    if (Array.isArray(grades))
-      return `${formatGrade(grades[0])}${nDash}${formatGrade(grades[1])}`
-    else if ('start' in grades && 'end' in grades)
-      return `${formatGrade(grades.start)}${nDash}${formatGrade(grades.end)}`
-    return nDash
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.collapsed !== this.props.collapsed) {
+      this.setState({collapsed: this.props.collapsed})
+    }
   }
 
-  const schoolTypeTag = (
-    <div className="level-item">
-      <div className="tags has-addons">
-        <span
-          className={'tag ' + (school.type === 'Elementary' ? 'is-light' : 'is-dark')}>
-          {school.type}
-        </span>
-        <span className="tag is-info">{makeGradesLabel(school.grades)}</span>
-      </div>
-    </div>
-  )
+  toggle() {
+    this.setState({collapsed: !this.state.collapsed});
+  }
 
-  const makeFraserTag = (school) => {
-    let colorClass = 'is-light'
-    let label = `Fr. ${nDash}`
-    if (school.fraser) {
-      const score = school.fraser.score
-      if (score >= 7.5)
-        colorClass = 'is-success'
-      else if (score < 7.5 && score >= 5.0)
-        colorClass = 'is-warning'
-      else
-        colorClass = 'is-danger'
-      label = `Fr. #${school.fraser.rank} (${school.fraser.score.toFixed(1)})`
+  makeQuickFacts(school) {
+    return (
+      <nav className="level my-2">
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">School Board</p>
+            <p className="title is-7">{school.school_board}</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">Opening Year</p>
+            <p className="title is-7">{school.year || NDASH}</p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">Fraser Rank</p>
+            <p className="title is-7">
+              {school.fraser ? '#' + Number(school.fraser.rank).toLocaleString() : NDASH}
+            </p>
+          </div>
+        </div>
+        <div className="level-item has-text-centered">
+          <div>
+            <p className="heading">Fraser Score</p>
+            <p className="title is-7">{school.fraser ? school.fraser.score.toFixed(1) : NDASH}</p>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+
+  makeExtraDetails(school) {
+    let fullAddress = NDASH;
+    if (school.address) {
+      fullAddress = `${school.address.address}, ${school.address.city}, ${school.address.postal_code}`;
+    }
+    let webLink = NDASH;
+    if (school.website) {
+      webLink = (
+        <a href={school.website} target="_blank" rel="noreferrer">
+          {school.website}
+        </a>
+      );
     }
     return (
-      <div className="tags has-addons">
-        <span className={'tag ' + colorClass}>{label}</span>
+      <nav className="level">
+        <div className="level-item is-justify-content-flex-start">
+          <p className="is-size-7"><FontAwesomeIcon icon={faMap} /> {fullAddress}</p>
+        </div>
+        <div className="level-item is-justify-content-flex-start">
+          <p className="is-size-7"><FontAwesomeIcon icon={faGlobe} /> {webLink}</p>
+        </div>
+      </nav>
+    )
+  }
+
+  makeTypeGradesTag(school) {
+
+    const makeGradesLabel = (grades) => {
+      const formatGrade = (grade) => (isNaN(grade) ? grade : _padStart(grade, 2, '0'));
+      if (Array.isArray(grades)) {
+        return `${formatGrade(grades[0])}${NDASH}${formatGrade(grades[1])}`;
+      }
+      else if ('start' in grades && 'end' in grades) {
+        return `${formatGrade(grades.start)}${NDASH}${formatGrade(grades.end)}`;
+      }
+      return NDASH;
+    };
+
+    return (
+      <div className="level-item">
+        <div className="tags has-addons">
+          <span
+            className={'tag ' + (school.type === 'Elementary' ? 'is-light' : 'is-dark')}>
+            {school.type}
+          </span>
+          <span className="tag is-info">{makeGradesLabel(school.grades)}</span>
+        </div>
       </div>
     )
   }
 
-  const fraserTagItem = !collapsed ? '' : (
-    <span className="ml-4">
-      {makeFraserTag(school)}
-    </span>
-  )
+  makeFraserTag(school) {
+    if (!this.state.collapsed) return null;
 
-  return (
-    <div className="box p-4 school-card">
-      <div className="level m-0">
-        <div className="level-left">
-          <div className="level-item pr-6">
-            <span className="title is-6">{school.name}</span>{fraserTagItem}
-          </div>
+    let colorClass = 'is-light';
+    let label = `Fr. ${NDASH}`;
+    if (school.fraser) {
+      const score = school.fraser.score;
+      if (score >= 7.5) {
+        colorClass = 'is-success';
+      }
+      else if (score < 7.5 && score >= 5.0) {
+        colorClass = 'is-warning';
+      }
+      else {
+        colorClass = 'is-danger';
+      }
+      label = `Fr. #${school.fraser.rank} (${school.fraser.score.toFixed(1)})`;
+    }
+    return (
+      <span className="ml-4">
+        <div className="tags has-addons">
+          <span className={'tag ' + colorClass}>{label}</span>
         </div>
-        <div className="level-right">
-          {/* {fraserTagItem} */}
-          <div className="level-item">
-            <small className="is-6_5">
-              <FontAwesomeIcon icon={faMapMarkerAlt} />  {school.address.city}
-            </small>
-          </div>
-          <div className="level-item">
-            {schoolTypeTag}
-          </div>
-          <div className="level-item">
-            <FontAwesomeIcon
-              className="toggle"
-              icon={collapsed ? faAngleLeft : faAngleDown}
-              onClick={toggle} />
-          </div>
-        </div>
+      </span>
+    );
+  }
+
+  render() {
+    const school = this.props.school;
+    if (!school) return null;
+    // Body parts
+    const quickFacts = this.makeQuickFacts(school);
+    const extraDetails = this.makeExtraDetails(school);
+    const cardBody = this.state.collapsed ? null : (
+      <div className="school-card-content">
+        <hr className="my-2" />
+        {quickFacts}
+        <hr className="my-2" />
+        {extraDetails}
       </div>
-      { cardContent }
-    </div>
-  )
+    );
+    // Header parts
+    const typeGradesTag = this.makeTypeGradesTag(school);
+    const fraserTag = this.makeFraserTag(school);
+
+    return (
+      <div className="box p-4 school-card">
+        <div className="level m-0">
+          <div className="level-left">
+            <div className="level-item pr-6">
+              <span className="title is-6">{school.name}</span>{fraserTag}
+            </div>
+          </div>
+          <div className="level-right">
+            <div className="level-item">
+              <small className="is-6_5">
+                <FontAwesomeIcon icon={faMapMarkerAlt} />  {school.address.city}
+              </small>
+            </div>
+            <div className="level-item">
+              {typeGradesTag}
+            </div>
+            <div className="level-item">
+              <FontAwesomeIcon
+                className="toggle"
+                icon={this.state.collapsed ? faAngleLeft : faAngleDown}
+                onClick={this.toggle} />
+            </div>
+          </div>
+        </div>
+        {cardBody}
+      </div>
+    );
+  }
 }
 
 
 SchoolCard.propTypes = {
   school: PropTypes.object,
   collapsed: PropTypes.bool
-}
-
-
-export default SchoolCard
+};
