47ed33b43aa34860c264f60a99969a0a556988d0
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-05 23:56:53 -0500

    Refactor sorting logic in utils/color.js.

--- src/utils/color.js
+++ src/utils/color.js
@@ -3,22 +3,19 @@ import * as d3Interpolate from 'd3-interpolate';
 import * as d3ScaleChromatic from 'd3-scale-chromatic';
 import _get from 'lodash.get';
 
+import {sortNullsLast} from './sort';
+
 
 const NULL_COLOR = 'gray'
 const COLOR_INTERPOLATOR = d3Interpolate.piecewise(
   d3Interpolate.interpolateRgb.gamma(2.2),
-  ["red", "orange", "green"]
+  ["green", "yellow", "red"]
 )
 
 
 const makeContiColorFunc = (range, field) => {
-  const sortedRange = range.sort((a, b) => {
-    if (a < b) return -1;
-    else if (a > b) return 1;
-    return 0;
-  })
-  const rangeStart = sortedRange[0]
-  const rangeEnd = sortedRange[sortedRange.length - 1]
+  const rangeStart = range[0]
+  const rangeEnd = range[range.length - 1]
   return (item) => {
     const fieldVal = _get(item, field, null)
     if (fieldVal === null)
@@ -29,45 +26,45 @@ const makeContiColorFunc = (range, field) => {
 }
 
 const makeCatColorFunc = (range, field) => {
-  const sortedRange = range.sort()
   return (item) => {
     const fieldVal = _get(item, field, null)
     if (fieldVal === null) {
       return NULL_COLOR
     }
     const palette = d3ScaleChromatic.schemeCategory10
-    return palette[sortedRange.indexOf(fieldVal)]
+    return palette[range.indexOf(fieldVal)]
   }
 }
 
 const makeMultiCatColorFunc = (range, field) => {
-  const sortedRange = range.sort()
   return (item) => {
     const fieldVal = _get(item, field, null)
     if (fieldVal === null) {
       return NULL_COLOR
     }
-    const fieldValNorm = sortedRange.indexOf(fieldVal) / (sortedRange.length - 1)
+    const fieldValNorm = range.indexOf(fieldVal) / (range.length - 1)
     return COLOR_INTERPOLATOR(fieldValNorm)
   }
 }
 
-const makeColorFunc = (data, field) => {
-  let colorFunc = (val) => null
-  if (!field)
-    return colorFunc
-  const fieldVals = data.map(item => _get(item, field, null))
+const makeColorFunc = (data, field, order) => {
+  let colorFunc = (val) => null;
+  if (!field) return colorFunc;
+
+  const fieldVals = data.map(item => _get(item, field, null));
   const fieldRange = [...new Set(fieldVals)]
-    .filter(item => item !== null)
+    .filter(item => item !== null);
+  const fieldRangeSorted = sortNullsLast(fieldRange, null, order);
   // Field considered non-numeric if at least one non-numeric value found
-  const numericScale = fieldRange.find(item => isNaN(item)) === undefined
-  if (numericScale)
-    colorFunc = makeContiColorFunc(fieldRange, field)
-  else if (fieldRange.length < 10)
-    colorFunc = makeCatColorFunc(fieldRange, field)
-  else
-    colorFunc = makeMultiCatColorFunc(fieldRange, field)
-  return colorFunc
+  const numericScale = fieldRangeSorted.find(item => isNaN(item)) === undefined;
+  if (numericScale) {
+    colorFunc = makeContiColorFunc(fieldRangeSorted, field);
+  } else if (fieldRangeSorted.length < 10) {
+    colorFunc = makeCatColorFunc(fieldRangeSorted, field);
+  } else {
+    colorFunc = makeMultiCatColorFunc(fieldRangeSorted, field);
+  }
+  return colorFunc;
 }
 
 const addOpacity = (color, opacity) => {
