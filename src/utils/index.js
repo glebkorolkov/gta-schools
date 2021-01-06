6a1aa1de9bf74b8742d3b3e887bacbc78eea1ee9
Gleb Korolkov <gkorolkov@loyalty.com>
2021-01-05 23:56:06 -0500

    Refactor utils/index.js by breaking sort functions into own module.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,4 @@
-import _get from 'lodash.get';
-
-import {makeColorFunc, addOpacity} from './color'
-
-
-const sortNullsLast = (arr, keyPath, order) => {
-  order = order || 'asc';
-  const mult = order.toLowerCase() === 'asc' ? 1 : -1;
-  let outArr = [...arr];
-  return outArr.sort((a, b) => {
-    const aVal = _get(a, keyPath, null);
-    const bVal = _get(b, keyPath, null);
-    if (aVal === null && bVal === null) return 0;
-    if (aVal === null) return 1;
-    if (bVal === null) return -1;
-    if (aVal < bVal) return (-1 * mult);
-    if (aVal > bVal) return (1 * mult);
-    return 0;
-  });
-};
-
+import {makeColorFunc, addOpacity} from './color';
+import {sortNullsLast} from './sort';
 
 export {sortNullsLast, makeColorFunc, addOpacity};
