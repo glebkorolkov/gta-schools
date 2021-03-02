290dcba5e8e8bdcbb927ce47b0a8c95d08d74fa2
Gleb Korolkov <gkorolkov@loyalty.com>
2021-03-02 17:31:53 -0500

    Update copy on AboutPage.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -8,6 +8,19 @@ import './AboutPage.scss';
 const AboutPage = (props) => {
   const defaultClasses = 'about-page has-background-white-ter';
   const containerClasses = props.className ? [defaultClasses, props.className].join(' ') : defaultClasses;
+  const fraserLink = (
+    <a href="https://www.compareschoolrankings.org/"
+      rel="noreferrer" target="_blank"
+      title="Fraser Institute">
+        Fraser Institute
+    </a>);
+  const eqaoLink = (
+    <a href="https://www.eqao.com/the-assessments/find-my-school/"
+    rel="noreferrer"
+    target="_blank"
+    title="Education Quality and Accountability Office">
+      EQAO
+    </a>);
   return (
     <div className={containerClasses}>
       <section className="hero">
@@ -19,26 +32,22 @@ const AboutPage = (props) => {
               </h1>
               <p>
                 This is an interactive map of schools in the Greater Toronto Area area. I built this
-                little website while choosing a school for my daughter.
+                little website while choosing a school for my child.
               </p>
               <p>
-                Information for the map was collected from public sources available on the Internet,
-                including school boards' websites and Fraser Institute.
-              </p>
-              <p>
-                It is a private project that I work on in my free time. I apologize in advance for
-                any bugs or inconsistencies.
+                Information for the map was collected from public sources on the Internet,
+                including school boards' websites, {fraserLink} and {eqaoLink}.
               </p>
               <h3 className="title is-4">Disclaimer</h3>
               <p>
-                <b>Information presented on the map may be incomplete or inaccurate</b>. It was
-                collected manually and programmatically from public sources on the Internet.
-                Do not rely on the information on this website and verify it with respective schools
-                and school boards before making decisions.
+                <b>Information presented on the website may be incomplete or inaccurate</b>. It was
+                collected manually and programmatically without extensive verification.
+                Do not rely on the information and verify it with respective schools
+                or school boards before making any decisions.
               </p>
               <p>
-                This website does not collect personal information, use cookies or generate any
-                revenue for the owner.
+                This website does not: collect personal information, use cookies or generate any
+                revenue for its owner.
               </p>
             </div>
             <div>
